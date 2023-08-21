cb7213947aa8e7a0eb37d34bf67f06c7ca811f66
Black1Matter <blackmatter19.70@gmail.com>
2023-08-21 08:13:02 +0200

    update snapshot

--- mainnet/Axelar/change.sh
+++ mainnet/Axelar/change.sh
@@ -2,30 +2,38 @@
 
 git pull
 
-json_file=$(curl -H GET https://s3.eu-central-1.amazonaws.com/w3coins.io/snapshots/cosmos-mainnet/cosmos_snapsot_latest.json | jq '.')
+index_str=3s
+data_link="https:\/\/s3.eu-central-1.amazonaws.com\/w3coins.io\/snapshots\/cosmos-mainnet\/cosmos_snapsot_latest.json"
 
+
+
+json_file=$(curl -H GET https://s3.eu-central-1.amazonaws.com/w3coins.io/snapshots/cosmos-mainnet/cosmos_snapsot_latest.json | jq '.')
 echo $json_file | jq '.'
 
 height=$(echo $json_file| jq -r '.latest_block_height')
 
 
+echo "Get time"
 start_seconds=$(echo $(echo $json_file| jq -r '.latest_block_time') | cut -d'T' -f2 | cut -d'.' -f1)
 end_seconds=$(echo $(echo $json_file| jq -r '.earliest_block_time') | cut -d'T' -f2 | cut -d'.' -f1)
-
 start_seconds=$(date -d "$start_time" +%s)
 end_seconds=$(date -d "$end_time" +%s)
-
 date_time=$(( (end_seconds - start_seconds) / 3600))
 
 echo $date_time
 
-data_link=https://s3.eu-central-1.amazonaws.com/w3coins.io/snapshots/cosmos-mainnet/cosmos_snapsot_latest.json
 
-sudo tee ./axelar.md > /dev/null << EOF
-|     Block   |     Age     |   Download  |
-| ----------- | ----------- | ----------- |
-|   $height   |  $date_time hour | [Snapshot]($data_link)  |
-EOF
+changed_str="|   $height   |  $date_time hour | [Snapshot]($data_link)  |"
+
+
+sed -i "$index_str/.*/$changed_str/" file.txt
+
+
+#sudo tee ./axelar.md > /dev/null << EOF
+#|     Block   |     Age     |   Download  |
+#| ----------- | ----------- | ----------- |
+#|   $height   |  $date_time hour | [Snapshot]($data_link)  |
+#EOF
 
 git add .
 git commit -m 'update snapshot'
