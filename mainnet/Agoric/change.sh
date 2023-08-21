7348859453af27a4d230b732e4262078c45a4920
Black1Matter <blackmatter19.70@gmail.com>
2023-08-21 10:57:48 +0200

    update

---
+++ mainnet/Agoric/change.sh
@@ -0,0 +1,39 @@
+# Axelar
+
+git pull
+
+index_str=3s
+data_link="https:\/\/s3.eu-central-1.amazonaws.com\/w3coins.io\/snapshots\/cosmos-mainnet\/cosmos_snapsot_latest.json"
+
+
+
+json_file=$(curl -H GET https://s3.eu-central-1.amazonaws.com/w3coins.io/snapshots/cosmos-mainnet/cosmos_snapsot_latest.json | jq '.')
+echo $json_file | jq '.'
+
+height=$(echo $json_file| jq -r '.latest_block_height')
+
+
+echo "Get time"
+start_seconds=$(echo $(echo $json_file| jq -r '.latest_block_time') | cut -d'T' -f2 | cut -d'.' -f1)
+end_seconds=$(echo $(echo $json_file| jq -r '.earliest_block_time') | cut -d'T' -f2 | cut -d'.' -f1)
+start_seconds=$(date -d "$start_time" +%s)
+end_seconds=$(date -d "$end_time" +%s)
+date_time=$(( (end_seconds - start_seconds) / 3600))
+
+echo $date_time
+
+
+changed_str="|   $height   |  $date_time hour | [Snapshot]($data_link)  |"
+
+
+sed -i "$index_str/.*/$changed_str/" snapshot.md
+
+#sudo tee ./axelar.md > /dev/null << EOF
+#|     Block   |     Age     |   Download  |
+#| ----------- | ----------- | ----------- |
+#|   $height   |  $date_time hour | [Snapshot]($data_link)  |
+#EOF
+
+git add .
+git commit -m 'update snapshot'
+git push
---
+++ mainnet/run.sh
@@ -0,0 +1,4 @@
+ssh -i /root/.ssh/id_rsa  'git pull'
+git add .
+git commit -m 'update'
+git push
